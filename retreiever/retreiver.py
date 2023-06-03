abeaad7b6b0fc07b748c3b4195e6c67fd7dc283e
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2023-06-02 18:53:22 -0700

    Loop through data and convert to array from string

---
+++ retreiever/retreiver.py
@@ -0,0 +1,14 @@
+import ast
+import pandas as pd
+
+df = pd.read_csv("288_data.csv")
+
+# Convert the string representation of a list into a list
+df['data'] = df['data'].apply(ast.literal_eval)
+
+for i in range(0, len(df['data'])):
+    print( df['data'][i] )
+    print( i )
+    # df['data'][i] = df['data'][i].split(',')
+
+
