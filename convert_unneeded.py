44cef9025d1f4936de39368e72e0455841c49dfe
Osani <jackahansen18@gmail.com>
2023-06-04 15:43:46 -0700

    removed unneeded

--- convert_unneeded.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-for filename in os.listdir():
-    if filename == "converter.py":
-        continue
-    if re.search("[^a-zA-Z0-9_.]+", filename):
-        newfile = re.sub("[^a-zA-Z0-9_.]+", "_", filename)
-        os.rename(filename, newfile)
