9f7305b4e9f665142f4d7ebc9049c2a12cfc1171
Bamboozle-jpg <kevin@bachelorclan.com>
2023-06-03 20:55:09 -0700

    Really high accuracy model

--- neuralNet/multiOutput.py
+++ neuralNet/multiOutput.py
@@ -6,6 +6,8 @@ import numpy as np
 from torch.utils.data import random_split, TensorDataset, DataLoader
 import math
 
+totalOutputs = 10
+
 globTrainLoss = []
 
 class generalModel(torch.nn.Module):
@@ -48,7 +50,7 @@ class generalModel(torch.nn.Module):
     # Function for training the model
     def trainn(self, numEpochs, trainLoader, validateLoader):
         lossFn = torch.nn.MSELoss()
-        optimizer = torch.optim.Adam(self.parameters(), lr=0.01, weight_decay=0.0001)
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.0001, weight_decay=0.001)
         bestAccuracy = 0.0
         
         print("Training with", numEpochs, "epochs...")
@@ -86,7 +88,7 @@ class generalModel(torch.nn.Module):
                     # Highest value will be our prediction
                     runningValLoss += valLoss.item()
                     for i in range(0, len(predictedOutputs[0])):
-                        if (abs(outputs[0][i] - predictedOutputs[0][i])/abs(outputs[0][i]) < .1):
+                        if (abs(outputs[0][i] - predictedOutputs[0][i])/10 < .1):
                             runningAccuracy += 1
                         total += 1
             
@@ -105,7 +107,7 @@ class generalModel(torch.nn.Module):
             globTrainLoss.append(trainLossValue)
             print("Completed training for epoch :", epoch, 'Training Loss is %.4f' %trainLossValue, 'Validation Loss is: %.4f' %valLossValue, 'Accuracy is %d %%' % (accuracy))
 
-    def test(self, testLoader, testSplit, outLength):
+    def test(self, testLoader, testSplit, outLength, columnName):
         runningAccuracy = 0
         total = 0
         checkingArray = [0 for i in range(outLength)]
@@ -113,33 +115,36 @@ class generalModel(torch.nn.Module):
         with torch.no_grad():
             for data in testLoader:
                 inputs, outputs = data
-                print("test inputs :", inputs)
-                print("test outputs :", outputs)
+                # print("test inputs :", inputs)
+                # print("test outputs :", outputs)
                 predictedOutputs = self(inputs)
-                print("predOuts : ", predictedOutputs)
+                # print("predOuts : ", predictedOutputs)
 
                 for i in range(0, len(outputs[0])):
-                    if (abs(outputs[0][i] - predictedOutputs[0][i])/abs(outputs[0][i]) < .1):
+                    if (abs(outputs[0][i] - predictedOutputs[0][i])/10 < .1):
                         runningAccuracy += 1
-                        print("i :", i)
-                        print("   pred : ", predictedOutputs[0][i])
-                        print("   actu : ", outputs[0][i])
+                        # print("i :", i)
+                        # print("   pred : ", predictedOutputs[0][i])
+                        # print("   actu : ", outputs[0][i])
                         checkingArray[i] += len(outputs[0])
                     total += 1
 
             print('Accuracy of the model based on the test set of', testSplit ,'inputs is: ',(100 * runningAccuracy / total), "%")
             print('Actual values :')
             for i in range(0, len(checkingArray)) : 
-                print("    " +str(i+1) + "th output's accuracy : " + str(100 * checkingArray[i]/total) + "%")
+                print("    " + columnName[i].ljust(16) + " : " + str(i+1).rjust(2) + "th output's accuracy : " + str(100 * checkingArray[i]/total) + "%")
 
 # Grabs data and turns it into usable form:
 df = pd.read_csv("../audioRead/SpotifyFeatures.csv")
 
+# Get rid of 0s FOR SOME REASON SOME OF THEM ARE CHARACTERS???
 df.drop(index=df.loc[df["data"] == 0].index, inplace=True)
 df.drop(index=df.loc[df["data"] == '0'].index, inplace=True)
 
+# Makes indices not skip over dropped rows and continue linearly
 df.reset_index(inplace=True)
 
+# Splits data frame into input and output
 output = df.loc[:, ["popularity","acousticness", "danceability", "energy","instrumentalness","liveness", "loudness", "speechiness","tempo", "valence"]]
 input = df.loc[:, ["data"]]
 
@@ -157,14 +162,34 @@ input = df.loc[:, ["data"]]
 # print(df["valence"][0].dtype)
 # print("\n\n\n")
 
-print("input dtpyes : ", input.dtypes)
+# Standardize all output values to be from 0 to 10
+print(output["loudness"])
+j = 0
+for i in output["loudness"]:
+    output["loudness"][j] = -1 * i
+    j+=1
+print(output["loudness"])
 
+i = 0
+for column in output:
+    maximum = max(output[column])
+    print(maximum)
+    for item in range(0, len(output[column])):
+        output[column][item] = 10*output[column][item]/maximum
+    i+=1
+
+# Gives testing a way to see the names for printing
+colNames = [""]*i
+i = 0
+for column in output:
+    colNames[i] = column
+    i+=1
+
+# Change the inputs from a string to an array
 for i in range(0, len(input["data"])):
     input["data"][i] = eval(input["data"][i])
 
-
-print("length of data array : ", len(input["data"][2]))
-
+# Turn the list of arrays into a numpy array so that the torch function takes it in properly
 inputArr = np.zeros((len(input["data"]), len(input["data"][0])))
 for i in range(0, len(input["data"])):
     for j in range(0, len(input["data"][0])):
@@ -180,7 +205,7 @@ outputSize = list(output.shape)[1]
 data = TensorDataset(input, output)
 
 # Split into a training, validation and testing set
-trainBatchSize = 10
+trainBatchSize = 50
 testSplit = int(len(input)*0.25)
 # print(testSplit)
 trainSplit = int(len(input)*0.6)
@@ -211,25 +236,24 @@ print("Output size :",outputSize)
 audioModel = generalModel(inputSize, outputSize)
 
 # Train model
-audioModel.trainn(140, trainLoader, validateLoader)
+audioModel.trainn(1024, trainLoader, validateLoader)
+audioModel.saveModel("finalTrain.pth")
 
 # Load best
-audioModel.loadModel(inputSize, outputSize, "bestInTrain.pth")
-
-
-audioModel.test(testLoader, testSplit, outputSize)
+# audioModel = audioModel.loadModel(inputSize, outputSize, "bestInTrain.pth")
+audioModel.test(testLoader, testSplit, outputSize, colNames)
 
 # To actually send something through, just call modelName.forward(input array)
 # If any of the values you want are not floats, you need to convert that, it will return all floats (or doubles? Not quite sure cuz python is silly)
 
-# # Analyze Training success w/ matplotlib
-# epochs = [i for i in range(1, len(globTrainLoss) + 1)]
-# fig = plt.figure(tight_layout=True)
-# ax = fig.add_subplot(2, 2, 2)
-# ax.plot(epochs, globTrainLoss, linewidth=1.5, markersize=0, color='purple')
-# ax.set_title("Training Loss")
-# ax.set_xlabel('Training Epoch')
-# ax.set_ylabel('Loss')
-# plt.show()
+# Analyze Training success w/ matplotlib
+epochs = [i for i in range(1, len(globTrainLoss) + 1)]
+ax = plt.figure(tight_layout=True)
+# ax = fig.add_subplot(2, 2, 1)
+ax.plot(epochs, globTrainLoss, linewidth=1.5, markersize=0, color='purple')
+ax.set_title("Training Loss")
+ax.set_xlabel('Training Epoch')
+ax.set_ylabel('Loss')
+ax.show()
 
 # Add in global variable so that every test you can plot it in matplotlib
